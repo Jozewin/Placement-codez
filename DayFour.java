98363c8835958d80c11780f734dcb4ba1d92dcb5
Jozewin <110077664+Jozewin@users.noreply.github.com>
2025-09-24 14:42:31 +0530

    Create DayFour.java

---
+++ DayFour.java
@@ -0,0 +1,55 @@
+public class DayFour {
+
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+
+        char originalChar = board[r][c];
+        board[r][c] = '#';
+
+        boolean found = dfs(board, word, r + 1, c, index + 1) ||
+                        dfs(board, word, r - 1, c, index + 1) ||
+                        dfs(board, word, r, c + 1, index + 1) ||
+                        dfs(board, word, r, c - 1, index + 1);
+
+        board[r][c] = originalChar;
+        return found;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        char[][] board = {
+            {'A','B','C','E'},
+            {'S','F','C','S'},
+            {'A','D','E','E'}
+        };
+        String word1 = "ABCCED";
+        String word2 = "SEE";
+        String word3 = "ABCB";
+
+        System.out.println(sol.exist(board, word1)); 
+        System.out.println(sol.exist(board, word2)); 
+        System.out.println(sol.exist(board, word3)); 
+    }
+}
